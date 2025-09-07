e8ee6174b1cd51f505678455e4a4e0a074087812
Caroline Marceano <CarolineMarceano@albassort.com>
2025-09-06 20:40:29 -0400

    Added a timeline and the btccode is working... mostly

--- src/btccode.nim
+++ src/btccode.nim
@@ -5,6 +5,7 @@ import NimBTC
 import Json
 import pretty
 import sequtils
+import sugar
 import tables
 import results
 import std/options
@@ -12,6 +13,8 @@ import os
 import libcurl
 import std/base64
 import times
+import streams
+import locks
 
 type HalfBtcResponse* = object
   error* : RpcErrorCode
@@ -34,6 +37,16 @@ type
     weight* : uint64
     lockTime* : uint64
     fee* : float64
+type 
+  CurlMessageBuffer = object
+    url : string
+    buffer : seq[string]
+    lock : Lock
+    #amount of pending messages
+    pCount : uint
+
+type State = enum
+  Poll, Reconfigure
 
 proc makeTx*(client : BTCClient, tx : string) : TX =
 
@@ -130,20 +143,19 @@ proc sendBTC*(client : BTCClient, outputs : Table[string, float], confTarget : u
   if submitted.isOK:
     return some makeTx(client, txid)
 
-proc initCurl(url : string, postBody = "", doPost = false) : PCurl =
+proc initCurl(url : string, resultStream : ptr CurlMessageBuffer, postBody = "", doPost = false) : PCurl =
 
-  proc curlWriteFn(buffer: cstring, size: int, count: int,outstream: pointer): int =
+  proc curlWriteFn(buffer: cstring, size: int, count: int, outstream: pointer): int =
 
-    # TODO: explain how this works because i forgor
-    let outbuf = cast[ref string](outstream)
-    outbuf[] &= buffer
-    echo buffer
-    result = size * count
+    let curlBuffer = cast[ptr CurlMessageBuffer](outstream)
+    withLock curlBuffer[].lock:
+      curlBuffer[].buffer.add($buffer)
+      curlBuffer[].pCount += 1 
+    return size * count
     
-  let webData: ref string = new string
   let curl = easy_init()
 
-  discard curl.easy_setopt(OPT_WRITEDATA, webData)
+  discard curl.easy_setopt(OPT_WRITEDATA, resultStream)
   discard curl.easy_setopt(OPT_WRITEFUNCTION, curlWriteFn)
   discard curl.easy_setopt(OPT_URL, url)
 
@@ -166,29 +178,98 @@ proc initCurl(url : string, postBody = "", doPost = false) : PCurl =
   return curl
 
 
+const libname = "libcurl.so(|.4)"
+proc multi_poll*(multi_handle: PM, skip : int, extra_nfds : uint32, timeout : int32, ret : var int32): Mcode{.cdecl,dynlib: libname, importc: "curl_multi_poll".}
+
 
 when isMainModule:
-  let curl1 = initCurl("https://localhost:8080/v1/invoices/subscribe")
+  let baseUrl = "https://localhost:8080/v1/"
+  let endPoints = @["invoices/subscribe", "channels/subscribe", "transactions/subscribe",
+                    "gra1h/subscribe",  "state/subscribe", "peers/subscribe"]
 
-  let curl2 = initCurl("https://localhost:8080/v1/channels/subscribe")
+  let endPointCount = endPoints.len
 
-  let multi = multi_init()
+  # Keeping theme out of the table for memory safety
+  var curlStreams : seq[CurlMessageBuffer]
+  # Maybe bad if the pointer can be realloced and moved.
+  var curlToStream : TableRef[PCurl, ptr CurlMessageBuffer] 
+  var multi : PM
+  var discon = 0
 
-  doAssert multi.multi_add_handle(curl1) == M_OK
-  doAssert multi.multi_add_handle(curl2) == M_OK
+  proc initEndPoints() =
+    for x in 0 .. curlStreams.high:
+      deinitLock curlStreams[x].lock
+    curlStreams.setLen(0)
 
+    curlToStream = newTable[PCurl, ptr CurlMessageBuffer]()
+
+    for endPoint in endPoints:
+      var curlStream : CurlMessageBuffer
+      initLock(curlStream.lock)
+      curlStreams.add(curlStream)
+
+      let point = addr curlStreams[curlStreams.high]
+      let newCurl = initCurl(baseUrl & endPoint, point)
+      curlToStream[newCurl] = point
+
+    multi = multi_init()
+    for curl in curlToStream.keys:
+       doAssert multi.multi_add_handle(curl) == M_OK
+
+  initEndPoints()
+  quit 1
   var stillRunning : int32
   doAssert multi_perform(multi, stillRunning) == M_OK
+  var count = 0
+  var state {.goto.} = Reconfigure
+  var ret : int32 = 0 
+  case state
+  of Poll:
+    while stillRunning == endPointCount:
 
-  echo stillRunning
-  # MAKE THE TOTAL NUMBER OF CONNECTIONS??
-  while stillRunning == 2:
-      # THis is what is used in the example but it was not wrapped.
-      #mc = curl_multi_poll(multi, NULL, 0, 1000, NULL);
-      echo "looomao"
-      doAssert multi_remove_handle(multi, curl1) == M_OK
-      doAssert multi_remove_handle(multi, curl2) == M_OK
+      doAssert multi_perform(multi, stillRunning) == M_OK
+      let poll = multi_poll(multi, 0, 0, 1000, ret)
 
-      easy_cleanup(curl1) 
-      easy_cleanup(curl2)
-      break
+      for i in 0 .. curlStreams.high: 
+        var curlStream = addr curlStreams[i]
+        if curlStream[].pCount != 0:
+          withLock curlStream[].lock:
+            for message in curlStream[].buffer:
+              echo (message, "MESSAGE")
+            curlStream[].buffer.setLen(0)
+            curlStream[].pCount = 0
+        
+      # if count == 10: 
+      #   count = 0
+      #   break
+      count += 1
+    # If that while loop isn't running, we can assume that 
+    # something disconnected or is having an issue connecting
+    state = Reconfigure
+  of Reconfigure:
+
+
+    var messagesLeft : int32 = 0
+
+    #TODO: figure out why mullti_info_read doesn't work on my system 
+
+    # doAssert multi_perform(multi, stillRunning) == M_OK
+    # let poll = multi_poll(multi, 0, 0, 1000, ret)
+    # echo poll
+    # while true:
+    #   let message = multi_info_read(multi, messagesLeft)
+    #   echo (messagesLeft, cast[int](message))
+    #   if message == nil: break
+    #   echo message[].msg
+    echo "Disonnection detected, disconnecting all endpoints and reconnecting"
+    discon += 1
+    for curl in curlToStream.keys:
+      doAssert multi_remove_handle(multi, curl) == M_OK
+      easy_cleanup(curl)
+      initEndPoints()
+
+    quit 1
+    state = Poll
+      # temp debug
+
+
